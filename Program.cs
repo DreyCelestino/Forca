a4ee38c66b76a3c504fa643c013c7db633dfd3c4
Andrey Celestino <lab53@fatec.com>
2025-03-17 10:50:47 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+String segredo;
+String letra;
+bool temounem;
+
+Console.WriteLine("--- Forca ---");
+Console.WriteLine(" ");
+Console.WriteLine("Escreva a palavra secreta: ");
+segredo = Console.ReadLine();
+
+Console.WriteLine("Escreva uma letra: ");
+letra = Console.ReadLine();
+
+temounem = segredo.Contains(letra);
+Console.WriteLine($"A letra '{letra}' existe na palavra secreta?");
+Console.WriteLine(temounem);
